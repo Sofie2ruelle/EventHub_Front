0480f1f6e7d2cff615581fef42618ee9bc16ff64
Hamza S <hamza@hotmail.hamza>
2023-02-24 14:35:21 +0100

    ajout login

--- EventHub/src/pages/Login.jsx
+++ EventHub/src/pages/Login.jsx
@@ -1,35 +1,73 @@
-export const Login = () => {
-    return (
+import axios from "axios";
+import { useState } from "react"
+import { UserDetail } from "./UserDetail";
 
-      <section className="bg-gray-50 dark:bg-white-900">
-        <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-        <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 bg-gradient-to-r from-pink-300 to-pink-600 dark:border-gray-700">
-        <div className="p-6 space-y-4 md:space-y-6 sm:p-8 ">
-            <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
-                Connectez-vous à votre commpte
-            </h1>
-            <form className="space-y-4 md:space-y-6" action="#">
-                <div>
-                    <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Votre email</label>
-                    <input type="email" name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" required=""/>
-                </div>
-                <div>
-                    <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Mot de passe</label>
-                    <input type="password" name="password" id="password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required=""/>
-                </div>
-                
-                <button type="submit" className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-pink-500 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:focus:ring-pink-800 hover:bg-sky-700">Se connecter</button>
-                <p className="text-sm font-bold text-white dark:text-white-400">
-                    Vous n'avez pas encore de compte ? <a href="#" className="font-medium text-primary-600 hover:underline dark:text-primary-500">Créer un compte</a>
-                </p>
-            </form>
-        </div>
-        </div>
-        </div>
-        </section>
+export const Login = () => {
+  const [email, setEmail] =useState('');
+  const [password, setPassword] =useState('');
+  const [error, setError] =useState('');
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    if(email == '' && password == ''){
+      setError("Champs vide")
+    }else{
+
+      axios
+    .post('http://localhost:8081/api/login', {email: email, password: password,})
+    .then(response => {
+      console.log(response.status)
+      if(response.data.succes){
+        setError('')
+
+        
+        console.log(response.data)
+
+      }else{
+        setError(response.data.message);
+      }
+    }).catch(error=>{
+      setError(error.response.data.message);
+      console.error(error);
+    })
+
+    }
+    
+    
+
+  }
+    return (
+      <section className="bg-gray-50 dark:bg-gray-900">
+  <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+      <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 bg-gradient-to-r from-pink-300 to-pink-600 dark:border-gray-700">
+          <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+              <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+                  Sign in to your account
+              </h1>
+              <form className="space-y-4 md:space-y-6" onSubmit={handleSubmit}>
+                  <div>
+                      <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Votre email</label>
+                      <input value={email} onChange={(event)=> setEmail(event.target.value)} type="email" name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" required=""/>
+                  </div>
+                  <div>
+                      <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
+                      <input value={password} onChange={(event)=> setPassword(event.target.value)} type="password" name="password" id="password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required=""/>
+                  </div>
+
+                  {error && <div className="font-medium text-blue-500 text-xxs">{error}</div>}
+
+                  <button type="submit" className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Sign in</button>
+                   
+                  
+                  <p className="text-sm font-light text-gray-500 dark:text-gray-400">
+                      Don’t have an account yet? <a href="#" className="font-medium text-primary-600 hover:underline dark:text-primary-500">Sign up</a>
+                  </p>
+              </form>
+          </div>
+      </div>
+  </div>
+</section>
     )
   }
-
-
-  
   
